a0fd6ae5d33a9fee2260cebc2da1b46ac810eb47
mjfdassen <mjfdassen@KANTOOR1>
2024-02-19 20:51:35 +0100

    Add project files.

---
+++ hoofdstuk3/First Drawing/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace First_Drawing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void drawButton_Click(object sender, RoutedEventArgs e)
+        {
+            Rectangle upperRectangle = new Rectangle();
+            upperRectangle.Width = 100;
+            upperRectangle.Height = 50;
+            upperRectangle.Margin = new Thickness(10, 10, 0, 0);
+            upperRectangle.Stroke = new SolidColorBrush(Colors.Black);
+            paperCanvas.Children.Add(upperRectangle);
+
+            Rectangle lowerRectangle = new Rectangle();
+            lowerRectangle.Width = 100;
+            lowerRectangle.Height = 100;
+            lowerRectangle.Margin = new Thickness(10, 75, 0, 0);
+            lowerRectangle.Stroke = new SolidColorBrush(Colors.Black);
+            paperCanvas.Children.Add(lowerRectangle);
+        }
+    }
+}
